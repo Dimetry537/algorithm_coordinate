7165d652f95b15cb8de910087b31c04f131c4c27
dimetry537 <maksakov1988@gmail.com>
2024-01-25 22:51:56 +0300

    add lines for polygon

--- src/polygon.py
+++ src/polygon.py
@@ -1,6 +1,6 @@
 import json
 import random
-import shapely.geometry
+from shapely.geometry import LineString, Polygon
 class MyPolygon:
 
     def __init__(self, geojson_polygon):
@@ -14,7 +14,7 @@ class MyPolygon:
 
     def _lats_and_lons(self):
         data = self._coordinates
-        polygon = shapely.geometry.Polygon(data)
+        polygon = Polygon(data)
         area = polygon.area
         lats = [coord[0] for coord in data]
         lons = [coord[1] for coord in data]
@@ -25,8 +25,16 @@ class MyPolygon:
         return lats_max, lons_max, lats_min, lons_min, area
     
     
+    def polygon_lines(self):
+        edges = []
+        for i in range(len(self._coordinates)):
+            start = self._coordinates[i]
+            end = self._coordinates[(i+1) % len(self._coordinates)]
+            edge = LineString([start, end])
+            edges.append(edge) 
+    
+    
     def ray_method(self, lats_max, lons_max, lats_min, lons_min, area):
-
         if area < 4:
             for i in range(100):
                 random_lat = random.randint(int(lats_min*100000), int(lats_max*100000))/100
