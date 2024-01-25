75be47e7c89db47f25348210332dcf9df7e4e594
dimetry537 <maksakov1988@gmail.com>
2024-01-25 22:43:09 +0300

    make polygon volume and random lat and lon

--- src/polygon.py
+++ src/polygon.py
@@ -1,8 +1,6 @@
 import json
-from shapely.geometry import Polygon
-import matplotlib.path as mplPath
-import numpy as np
-
+import random
+import shapely.geometry
 class MyPolygon:
 
     def __init__(self, geojson_polygon):
@@ -13,38 +11,30 @@ class MyPolygon:
             raise ValueError("Недостаточно точек для построения полигона (минимум 3)")
 
         self._coordinates = coordinates[0]
-        self._polygon = Polygon(self._coordinates)
-        inner_coordinates = self.get_inner_coords()
-        MyPolygon.all_coordinates.append(self._coordinates)
-        MyPolygon.all_coordinates.append(inner_coordinates)
+
+    def _lats_and_lons(self):
+        data = self._coordinates
+        polygon = shapely.geometry.Polygon(data)
+        area = polygon.area
+        lats = [coord[0] for coord in data]
+        lons = [coord[1] for coord in data]
+        lats_max = max(lats)
+        lons_max = max(lons)
+        lats_min = min(lats)
+        lons_min = min(lons)
+        return lats_max, lons_max, lats_min, lons_min, area
     
-    @property
-    def polygon(self):
-        return self._polygon
-
-    def get_inner_coords(self):
-        path = mplPath.Path(self._coordinates)
-
-        min_x = np.floor(min(p[0] for p in self._coordinates))
-        min_y = np.floor(min(p[1] for p in self._coordinates)) 
-
-        max_x = np.ceil(max(p[0] for p in self._coordinates))
-        max_y = np.ceil(max(p[1] for p in self._coordinates))
-
-        x_coords = np.arange(min_x, max_x, 0.1)
-        y_coords = np.arange(min_y, max_y, 0.1)
-
-        inner_coords = []
-        for x in x_coords:
-            for y in y_coords:
-                if path.contains_point((x, y)):
-                    inner_coords.append([x, y])
-
-        return inner_coords
     
-    @staticmethod
-    def get_all_coordinates():
-        return MyPolygon.all_coordinates
-    
-    def __repr__(self) -> str:
-        return f"MyPolygon({self._coordinates})"
+    def ray_method(self, lats_max, lons_max, lats_min, lons_min, area):
+
+        if area < 4:
+            for i in range(100):
+                random_lat = random.randint(int(lats_min*100000), int(lats_max*100000))/100
+                random_lon = random.randint(int(lons_min*100000), int(lons_max*100000))/100
+                yield random_lat, random_lon
+        else:
+            for i in range(1000):
+                random_lat = random.randint(int(lats_min*100000), int(lats_max*100000))/100
+                random_lon = random.randint(int(lons_min*100000), int(lons_max*100000))/100
+                yield random_lat, random_lon
+
