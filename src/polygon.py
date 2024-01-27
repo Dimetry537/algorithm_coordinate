f3b6081613c1307c818610a228955506233478c8
dimetry537 <maksakov1988@gmail.com>
2024-01-27 20:26:30 +0300

    add all polygon coords

--- src/polygon.py
+++ src/polygon.py
@@ -1,6 +1,7 @@
 import json
 import random
 from shapely.geometry import LineString, Polygon
+from shapely import intersects
 class MyPolygon:
 
     def __init__(self, geojson_polygon):
@@ -31,18 +32,26 @@ class MyPolygon:
             start = self._coordinates[i]
             end = self._coordinates[(i+1) % len(self._coordinates)]
             edge = LineString([start, end])
-            edges.append(edge) 
+            edges.append(edge)
+            return edges
     
     
-    def ray_method(self, lats_max, lons_max, lats_min, lons_min, area):
+    def ray_method(self, lats_max, lons_max, lats_min, lons_min, area, edges):
         if area < 4:
-            for i in range(100):
-                random_lat = random.randint(int(lats_min*100000), int(lats_max*100000))/100
-                random_lon = random.randint(int(lons_min*100000), int(lons_max*100000))/100
-                yield random_lat, random_lon
+            iterations = 500
         else:
-            for i in range(1000):
-                random_lat = random.randint(int(lats_min*100000), int(lats_max*100000))/100
-                random_lon = random.randint(int(lons_min*100000), int(lons_max*100000))/100
-                yield random_lat, random_lon
+            iterations = 1000
 
+        for i in range(iterations):
+                random_lat = random.randint(int(lats_min*100000), int(lats_max*100000))/100000
+                random_lon = random.randint(int(lons_min*100000), int(lons_max*100000))/100000
+                point = (random_lat, random_lon)
+                horizontal_line = LineString([point, (random_lon + 1, random_lat + 1)])
+                intersections = 0
+                for edge in edges:
+                    if edge.intersects(horizontal_line):
+                        intersections += 1
+
+                if intersections %2 != 0:
+                    yield random_lat, random_lon
+                    continue
