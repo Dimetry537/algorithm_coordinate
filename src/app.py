f9af08142a64a9c367fa8ec0c239587a151d4301
dimetry537 <maksakov1988@gmail.com>
2024-02-04 20:05:37 +0300

    complete json decoder for polygons & points

--- src/app.py
+++ src/app.py
@@ -3,24 +3,21 @@ from json_encoder import JsonEncoder
 from my_polygon import MyPolygon
 from graph_executor import GraphExecutor
 
-start_point = JsonEncoder(start_point)
-end_point = JsonEncoder(end_point)
-first_polygon = JsonEncoder(first_polygon)
-second_polygon = JsonEncoder(second_polygon)
-third_polygon = JsonEncoder(third_polygon)
-
-start_point_coords = start_point.decode()
-end_point_coords = end_point.decode()
-first_polygon_coords = first_polygon.decode()
-second_polygon_coords = second_polygon.decode()
-third_polygon_coords = third_polygon.decode()
+json = JsonEncoder()
 # polygon = MyPolygon()
 # graph_executor = GraphExecutor()
-print(start_point_coords)
-print(end_point_coords)
-print(first_polygon_coords)
-print(second_polygon_coords)
-print(third_polygon_coords)
+
+start_point = json.decode_points(start_point)
+end_point = json.decode_points(end_point)
+first_polygon = json.decode_polygon(first_polygon)
+second_polygon = json.decode_polygon(second_polygon)
+third_polygon = json.decode_polygon(third_polygon)
+
+print(start_point)
+print(end_point)
+print(first_polygon)
+print(second_polygon)
+print(third_polygon)
 
 # all_coords = [first_polygon, second_polygon, third_polygon, start_point, end_point]
 
--- src/json_encoder.py
+++ src/json_encoder.py
@@ -1,17 +1,23 @@
 import json
 
 class JsonEncoder:
-    def __init__(self, data):
+    def decode_points(self, data):
+        self.data = json.loads(data)
+        self.coordinates = self.data['coordinates']
+        coordinates = tuple(self.coordinates)
+        return coordinates
+    
+    
+    def decode_polygon(self, data):
         self.data = json.loads(data)
         self.coordinates = self.data['coordinates'][0]
-
-    def decode(self):
         coordinates = [(coords[0], coords[1]) for coords in self.coordinates]
-        return [coordinates]
+        return coordinates
     
-    def encode_line_string(self, coordinates):
-        line_string = {
-            "type": "LineString",
-            "coordinates": coordinates
-        }
-        return self.data_encoded(line_string)
+    
+    # def encode_line_string(self, coordinates):
+    #     line_string = {
+    #         "type": "LineString",
+    #         "coordinates": coordinates
+    #     }
+    #     return self.data_encoded(line_string)
